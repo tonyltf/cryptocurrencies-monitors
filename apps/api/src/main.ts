be5174c27a405cb97eef1d21ace6f1f4d78efec0
tony.li <tony@choco-up.com>
2023-02-24 00:37:11 +0800

    docs(api): add Swagger doc to API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cryptocurrency monitor API')
+    .setDescription('The API document')
+    .setVersion('1.0')
+    .addTag('crypto')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8000);
 }
 bootstrap();
