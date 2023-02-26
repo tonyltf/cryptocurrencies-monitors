7e45baff4f3f59f1a43a7d7e928c76c11e93d27f
tony.li <tony@choco-up.com>
2023-02-27 00:40:02 +0800

    style(package/ui): run prettier

--- packages/ui/Card.tsx
+++ packages/ui/Card.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import styled from 'styled-components'
-import styles from './flex.module.css';
+import styled from "styled-components";
+import styles from "./flex.module.css";
 
-const CardWrapper = styled.div<{ height?: number; width?: number; }>`
-  border: 1px solid #AAA;
+const CardWrapper = styled.div<{ height?: number; width?: number }>`
+  border: 1px solid #aaa;
   padding: 1em;
-  width: ${({width}) => width ? width+'px' : 'auto'};
-  max-width: ${({width}) => width ? width+'px' : 'auto'};
-  height: ${({height}) => height ? height+'px' : 'auto'};
+  width: ${({ width }) => (width ? width + "px" : "auto")};
+  max-width: ${({ width }) => (width ? width + "px" : "auto")};
+  height: ${({ height }) => (height ? height + "px" : "auto")};
 `;
 
 const CardTitle = styled.div`
@@ -22,27 +22,41 @@ const ItemContainer = styled.div`
   display: flex;
 `;
 
-const ItemWrapper = styled.div`
-`;
+const ItemWrapper = styled.div``;
 
-const ItemLabel = styled.div`
-`;
+const ItemLabel = styled.div``;
 
-const ItemValue= styled.div<{ color: string; }>`
+const ItemValue = styled.div<{ color: string }>`
   color: ${({ color }) => color};
 `;
 
-export const Card = ({ height, width, title, subtitle, items }: { height?: number; width?: number; title: string; subtitle: string; items: { label: string; value: string; color?: string; }[] }) => {
-  return <CardWrapper className={styles.child} height={height} width={width}>
-    <CardTitle>{title}</CardTitle>
-    <CardSubtitle>{subtitle}</CardSubtitle>
-    <ItemContainer className={styles.parent}>
-      {items?.map(({ label, value, color }) => {
-        return <ItemWrapper key={label} className={styles.child}>
-          <ItemLabel>{label}</ItemLabel>
-          <ItemValue color={color || 'black'}>{value}</ItemValue>
-        </ItemWrapper>
-      })}
-    </ItemContainer>
-  </CardWrapper>;
+export const Card = ({
+  height,
+  width,
+  title,
+  subtitle,
+  items,
+}: {
+  height?: number;
+  width?: number;
+  title: string;
+  subtitle: string;
+  items: { label: string; value: string; color?: string }[];
+}) => {
+  return (
+    <CardWrapper className={styles.child} height={height} width={width}>
+      <CardTitle>{title}</CardTitle>
+      <CardSubtitle>{subtitle}</CardSubtitle>
+      <ItemContainer className={styles.parent}>
+        {items?.map(({ label, value, color }) => {
+          return (
+            <ItemWrapper key={label} className={styles.child}>
+              <ItemLabel>{label}</ItemLabel>
+              <ItemValue color={color || "black"}>{value}</ItemValue>
+            </ItemWrapper>
+          );
+        })}
+      </ItemContainer>
+    </CardWrapper>
+  );
 };
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,3 +1,3 @@
 import * as React from "react";
 export * from "./Card";
-export * as styles from './flex.module.css';
+export * as styles from "./flex.module.css";
